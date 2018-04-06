b0efc2eed23f7b742a7bb3acf7e72eea61cd5337
Travis Witherington <twitherington@covermymeds.com>
2018-04-06 14:42:17 -0500

    registration confirmation update

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -6,15 +6,17 @@ class RegistrationsController < Devise::RegistrationsController
       resource.save
       yield resource if block_given?
       if resource.persisted?
-        @payment = Payment.new({email: params['user']['email'],
-                                token: params[:payment]['token'], user_id: resource.id})
-        flash[:error] = 'Please check registration errors' unless @payment.valid?
+        @payment = Payment.new({ email: params["user"]["email"],
+                                 token: params[:payment]["token"], user_id: resource.id })
+
+        flash[:error] = "Please check registration errors" unless @payment.valid?
 
         begin
           @payment.process_payment
           @payment.save
         rescue Exception => e
           flash[:error] = e.message
+
           resource.destroy
           puts 'Payment failed'
           render :new and return
